f1cd9ef647ac1fbc71cfba643ce150e71767e35c
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-12-17 23:12:45 +0600

    Design: Header

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Hello World!! 123</div>;
+  return (
+    <div>
+      <h1>Hello World!! 123</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 }
