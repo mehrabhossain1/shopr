8a5f8dc4504095f569b976761060dbb1d895675f
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-12-18 13:28:29 +0600

    Helper function: getAllProducts

---
+++ components/ProductsView.tsx
@@ -0,0 +1,27 @@
+import { Product } from "@/sanity.types";
+
+interface ProductsViewProps {
+  products: Product[];
+}
+
+const ProductsView = ({ products }: ProductsViewProps) => {
+  return (
+    <div className="flex flex-col">
+      {/* categories */}
+      <div className="w-full sm:w-[200px]">
+        {/* <CategorySelectorComponent categories={categories} /> */}
+      </div>
+
+      {/* products */}
+      <div className="flex-1">
+        <div>
+          {/* <ProductGrid  products={products} /> */}
+
+          <hr className="w-1/2 sm:w-3/4" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsView;
