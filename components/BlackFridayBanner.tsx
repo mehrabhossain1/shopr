c391101ccc6ba004881e101cbbf1d9bc92434d28
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-12-19 19:19:54 +0600

    Search Page functionality

--- components/BlackFridayBanner.tsx
+++ components/BlackFridayBanner.tsx
@@ -9,10 +9,7 @@ async function BlackFridayBanner() {
   }
 
   return (
-    <div
-      className="bg-gradient-to-r from-red-600 to-black text-white px-6 py-10 mx-4
-    rounded-lg shadow-lg"
-    >
+    <div className="bg-gradient-to-r from-red-600 to-black text-white px-6 py-10 mx-4 rounded-lg shadow-lg">
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex-1">
           <h2 className="text-3xl sm:text-5xl font-extrabold text-left mb-4">
